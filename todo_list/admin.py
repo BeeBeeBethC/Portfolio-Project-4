248d69c202d0a131afe3288ee6a5eeeb2d88af26
Bethan Cottell <cottellbc@gmail.com>
2024-11-22 11:28:58 +0000

    adjusted person to todo in admin.py

--- todo_list/admin.py
+++ todo_list/admin.py
@@ -1,14 +1,14 @@
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
-from .models import person
+from .models import ToDo
 
 # Register your models here.
 
 
-@admin.register(person)
+@admin.register(ToDo)
 class ToDoAdmin(SummernoteModelAdmin):
 
-    list_display = ('username', 'firstname', 'lastname', 'email', 'is_staff')
-    search_fields = ['username', 'email', 'is_staff']
+    list_display = ('add_item', 'confirm', 'created_at')
+    search_fields = ['created_at']
     # list_filter = ('status',)
     summernote_fields = ('content',)
